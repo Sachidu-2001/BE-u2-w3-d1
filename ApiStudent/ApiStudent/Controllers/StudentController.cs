66b28fa7c1b2be963bd6f7d58a52ec5146baea3f
Sachidu-2001 <fsachi0001@gmail.com>
2025-03-24 15:36:42 +0100

    commit

---
+++ ApiStudent/ApiStudent/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiStudent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly StudentService
+    }
+}
---
+++ ApiStudent/ApiStudent/Services/StudentService.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using ApiStudent.Data;
+using ApiStudent.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiStudent.Services
+{
+    public class StudentService
+    {
+        private ApplicationDbContext _context;
+
+        public StudentService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        private async Task<bool> SaveAsync()
+        {
+            try
+            {
+                return await _context.SaveChangesAsync() > 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> CreateStudentAsync(Student student)
+        {
+            try
+            {
+                _context.Students.Add(student);
+                return await SaveAsync();
+            }
+            catch
+            { 
+                return false;
+            }
+
+        }
+
+        public async Task<List<Student>?> GetStudentsAsync()
+        {
+            try
+            {
+                return await _context.Students.ToListAsync();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
