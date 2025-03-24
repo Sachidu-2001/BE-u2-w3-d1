18e25d80d673174b41567d217fb925aba251a1e6
Sachidu-2001 <fsachi0001@gmail.com>
2025-03-24 14:24:34 +0100

    commit

---
+++ ApiStudent/ApiStudent/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiStudent.Models
+{
+    public class Student
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Cognome { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
